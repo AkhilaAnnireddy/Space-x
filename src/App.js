e06db78240ba302dfb93b9dc0fee72eed989eb9e
Akhila Annireddy <akhilaannireddy0605@gmail.com>
2020-12-12 01:06:50 -0800

    spacex upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Header } from './components/Header/Header';
+import './styles.css'
+import { LaunchList } from './components/LaunchList/LaunchList';
+import LaunchView from './components/LaunchView/LaunchView';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/launch/:flight_number">
+            <LaunchView />
+          </Route>
+          <Route path="/">
+            <LaunchList />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
